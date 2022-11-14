35e2a3c9f844422fee9744720451de7852726677
ahracho <ahra123@i-commtech.com>
2022-11-14 18:01:20 +0900

    다중 보안 클래스 추가

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security.basicsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,55 +29,80 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Order(0)     //설정클래스 초기화하는 순서 (요청 순서 : 순서에 따라서 자원 접근 허용이 달라짐 / 넓은 범위의 요청 순서가 우선순위가 낮아야함)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {  //첫번쪠 시큐리티 설정 클래스
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
-        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
-//        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");    //각기 다른 권한의 계정 생성
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");    //admin 계정이 다른 권한에 해당하는 자원의 접근을 가능하게 함
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/admin/**")   //특정 url 설정
+                .authorizeRequests()
+                .anyRequest().authenticated()     //모든 사용자가 인증을 받아야만 자원에 접근가능 (authenticated)
+                .and()
+                .httpBasic();   //인증방식 - 방식에 따라서 필터의 구성이 다름
     }
+}//인증방식
+
+@Configuration
+@Order(1)
+class SecurityConfig2 extends WebSecurityConfigurerAdapter {  //두번쩨 시큐리티 설정 클래스
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll()    //인증을 받지 않아도 접근 가능 (permitAll)
+                .and()
+                .formLogin();    //인증방식
+    }
+
+}
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+//        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+//        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");    //각기 다른 권한의 계정 생성
+//        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");    //admin 계정이 다른 권한에 해당하는 자원의 접근을 가능하게 함
+//    }
 
     //    @Autowired
 //    UserDetailsService userDetailsService;
 //
 //    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/login").permitAll()     //로그인 페이지 접근 권한 설정 해제
-                .antMatchers("/user").hasRole("USER")     //특정 요청에 대해서 인가정책을 매치 (권한 심사)
-                .antMatchers("/admin/pay").hasRole("ADMIN")
-                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")      // 권한 설정에 순서도 중요(전체 권한 먼저 설정후 부분 권한 설정을 하면 안됨)
-                .anyRequest().authenticated();   //요청에 대한 보안검색 시작(모든 요청에 대해서 인증을 받도록 설정 - 인가정책)
-
-        http
-                .formLogin()       //인증정책(formlogin을 통해서)
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        RequestCache requestCache = new HttpSessionRequestCache();     //세션에 저장되어있는 이전의 정보 객체
-                        SavedRequest savedRequest = requestCache.getRequest(request, response);   //세션에 저장되어있는 요청과 응답 객체
-                        String redirectUrl = savedRequest.getRedirectUrl();
-                        response.sendRedirect(redirectUrl);    //세션에 저장되어있는 redirectUrl로 리다이렉트
-                    }
-                });
-
-        http
-                .exceptionHandling()
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/login").permitAll()     //로그인 페이지 접근 권한 설정 해제
+//                .antMatchers("/user").hasRole("USER")     //특정 요청에 대해서 인가정책을 매치 (권한 심사)
+//                .antMatchers("/admin/pay").hasRole("ADMIN")
+//                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")      // 권한 설정에 순서도 중요(전체 권한 먼저 설정후 부분 권한 설정을 하면 안됨)
+//                .anyRequest().authenticated();   //요청에 대한 보안검색 시작(모든 요청에 대해서 인증을 받도록 설정 - 인가정책)
+//
+//        http
+//                .formLogin();       //인증정책(formlogin을 통해서)
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        RequestCache requestCache = new HttpSessionRequestCache();     //세션에 저장되어있는 이전의 정보 객체
+//                        SavedRequest savedRequest = requestCache.getRequest(request, response);   //세션에 저장되어있는 요청과 응답 객체
+//                        String redirectUrl = savedRequest.getRedirectUrl();
+//                        response.sendRedirect(redirectUrl);    //세션에 저장되어있는 redirectUrl로 리다이렉트
+//                    }
+//                });
+//
+//        http
+//                .exceptionHandling()
 //                .authenticationEntryPoint(new AuthenticationEntryPoint() {
 //                    @Override
 //                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 //                        response.sendRedirect("/login");
 //                    }
 //                })
-                .accessDeniedHandler(new AccessDeniedHandler() {
-                    @Override
-                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                        response.sendRedirect("/denied");
-                    }
-                });
+//                .accessDeniedHandler(new AccessDeniedHandler() {
+//                    @Override
+//                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+//                        response.sendRedirect("/denied");
+//                    }
+//                });
 
 //        http
 //                .sessionManagement()
@@ -134,5 +160,3 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .rememberMeParameter("remember")
 //                .tokenValiditySeconds(3600)
 //                .userDetailsService(userDetailsService);
-    }
-}
